daf81a783e3d95ebb0b7a52401bb67c8f6936349
Adi <shelkeadinath3@gmail.com>
2023-09-15 08:41:08 +0530

    sorting all the fetched users based on the employee name

--- src/routes/allot.ts
+++ src/routes/allot.ts
@@ -4,7 +4,7 @@ const router = express.Router()
 router.post("/:course_id", async (req, res) => {
     const courseId = req.params.course_id
     try {
-        const registeredUsers = await CourseEnrollments.find({ course_id: courseId })
+        const registeredUsers = await CourseEnrollments.find({ course_id: courseId }).sort({_id:1})
         res.json(registeredUsers)
     } catch (error) {
         console.log(error)
